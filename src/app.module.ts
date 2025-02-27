7e8f3a40653ec0abf622535147f07eeb2a5170b0
Hugo Lasserre <hugo.lasserrecab@gmail.com>
2025-02-27 15:56:07 +0100

    feat ajout des modules Table et SeatingPlan, mise à jour des dépendances et ajout du DTO UpdateTable

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import 'dotenv/config';
 import { Meal } from './entities/Meal';
+import { TableModule } from './service/table/table.module';
+import { SeatingPlanModule } from './service/seating-plan/seating-plan.module';
+
 @Module({
   imports: [
+    TableModule,
+    SeatingPlanModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
