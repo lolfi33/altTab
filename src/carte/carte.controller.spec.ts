78eb75105e30bcdd431105864480810752a1a3a9
Ugo SH <ugosh16@gmail.com>
2025-02-27 16:44:37 +0100

    ajout tests

--- src/carte/carte.controller.spec.ts
+++ src/carte/carte.controller.spec.ts
@@ -1,18 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CarteController } from './carte.controller';
+import Meal from './entities/Meal';
+import { CarteService } from './carte.service';
+import { MealType } from './interfaces/mealType';
 
 describe('CarteController', () => {
-  let controller: CarteController;
+  describe('PlatController', () => {
+    let controller: CarteController;
+    let service: CarteService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CarteController],
-    }).compile();
+    beforeEach(async () => {
+      const mockService = {
+        getAll: jest.fn(),
+        ajouterPlat: jest.fn(),
+      };
 
-    controller = module.get<CarteController>(CarteController);
-  });
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [CarteController],
+        providers: [
+          {
+            provide: CarteService,
+            useValue: mockService,
+          },
+        ],
+      }).compile();
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+      controller = module.get<CarteController>(CarteController);
+      service = module.get<CarteService>(CarteService);
+    });
+
+    describe('getAll', () => {
+      it('doit retourner un tableau de plats', async () => {
+        const meals: Meal[] = [
+          {
+            id: 1,
+            name: 'Tarte aux pommes',
+            description: 'Délicieuse tarte',
+            type: MealType.DESSERT,
+            price: 5.5,
+            quantity: 0,
+          },
+          {
+            id: 2,
+            name: 'Tarte aux cerises',
+            description: 'Délicieuse tarte',
+            type: MealType.DESSERT,
+            price: 5,
+            quantity: 2,
+          },
+        ];
+
+        jest.spyOn(service, 'getAll').mockResolvedValue(meals);
+
+        const result = await controller.getAll();
+
+        expect(result).toEqual(meals);
+        expect(result.length).toBe(2);
+      });
+    });
   });
 });
--- src/carte/carte.service.spec.ts
+++ src/carte/carte.service.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CarteService } from './carte.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Meal } from './entities/Meal';
 
 describe('CarteService', () => {
   let service: CarteService;
+  let repository: any;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CarteService],
+      providers: [
+        CarteService,
+        {
+          provide: getRepositoryToken(Meal),
+          useValue: {
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CarteService>(CarteService);
+    repository = module.get(getRepositoryToken(Meal));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getAllPlats', () => {
+    it('doit récupérer tous les plats', async () => {
+      const meals = [
+        {
+          id: 1,
+          name: 'Tarte aux pommes',
+          description: 'Délicieuse tarte',
+          type: 'Dessert',
+          price: 5.5,
+          quantity: 0,
+        },
+        {
+          id: 2,
+          name: 'Tarte aux cerises',
+          description: 'Délicieuse tarte',
+          type: 'Dessert',
+          price: 5,
+          quantity: 2,
+        },
+      ];
+
+      repository.find = jest.fn().mockReturnValue(meals);
+
+      const result = await service.getAll();
+
+      expect(repository.find).toHaveBeenCalled();
+      expect(result).toEqual(meals);
+      expect(result.length).toEqual(2);
+    });
   });
 });
