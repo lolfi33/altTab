eb3ba61607ff7bf5d010f2c04993eca9b9644ac9
Tristan Petit <tristan.petit01@gmail.com>
2025-02-27 14:10:42 +0100

    ajout d'un plat fonctionnel

--- src/carte/carte.service.ts
+++ src/carte/carte.service.ts
@@ -18,14 +18,7 @@ export class CarteService {
     if (existingMeal) {
       throw new BadRequestException('Un plat avec ce nom existe déjà.');
     }
-
-    // Arrondi du prix au centime (si besoin, bien que le type "decimal" dans la BDD puisse s'en charger)
     createMealDto.price = Math.round(createMealDto.price * 100) / 100;
-
-    // Si la quantité n'est pas fournie, on garde la valeur par défaut (0)
-    if (typeof createMealDto.quantity === 'undefined') {
-      createMealDto.quantity = 0;
-    }
 
     const meal = this.mealRepository.create(createMealDto);
     return this.mealRepository.save(meal);
