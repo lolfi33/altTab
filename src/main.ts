1cb609fd2909f4dfd959d118c1ea3098b16a4878
lolfi33 <yoayoann@gmail.com>
2025-02-28 12:39:06 +0100

    changement du port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   // Port du serveur sinon 3000
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
