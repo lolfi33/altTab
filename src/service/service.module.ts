39e9696ac85c46572a64b10f2b639fdffb330fff
Tristan Petit <tristan.petit01@gmail.com>
2025-02-28 15:47:42 +0100

    fix : module dépendance

--- src/service/service.module.ts
+++ src/service/service.module.ts
@@ -2,8 +2,12 @@ import { SeatingPlanService } from './seating-plan/seating-plan.service';
 import { Module } from '@nestjs/common';
 import { ServiceController } from './service.controller';
 import { ServiceService } from './service.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SeatingPlan } from './seating-plan/entities/seating-plan.entity';
+import { TableModule } from './table/table.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SeatingPlan]), TableModule],
   controllers: [ServiceController],
   providers: [ServiceService, SeatingPlanService],
 })
