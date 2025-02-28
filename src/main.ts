e2de124670aa3ba2a54037e9744bc5dce272842b
lolfi33 <yoayoann@gmail.com>
2025-02-28 12:11:13 +0100

    push le main !

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Port du serveur sinon 3000
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
